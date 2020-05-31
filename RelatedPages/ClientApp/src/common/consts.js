06ca85175be32d48fd7c60503f15a4409fcedd6f
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-05-31 09:06:21 +0900

    Update consts.js

--- RelatedPages/ClientApp/src/common/consts.js
+++ RelatedPages/ClientApp/src/common/consts.js
@@ -1,2 +1,2 @@
-export const siteUrl = "https://pages.lingual-ninja.com";
-export const azureUrl = "https://relatedpages20200524124530.azurewebsites.net";
+export const siteUrl = "pages.lingual-ninja.com";
+export const azureUrl = "relatedpages20200524124530.azurewebsites.net";
